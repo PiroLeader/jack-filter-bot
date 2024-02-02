74754d4b265ddf2d8cd351fabd8933a6dc8690a1
GamingBeast1 <116881154+GamingBeast1@users.noreply.github.com>
2024-02-02 20:09:14 +0530

    Update pmfilter.py

--- plugins/pmfilter.py
+++ plugins/pmfilter.py
@@ -1836,6 +1836,12 @@ async def cb_handler(client: Client, query: CallbackQuery):
             query.message.id, 
             InputMediaPhoto(random.choice(PICS))
         )
+        reply_markup = InlineKeyboardMarkup(buttons)
+        await query.message.edit_text(
+            text=script.EXTRA_FEATURE_TXT,
+            reply_markup=reply_markup,
+            parse_mode=enums.ParseMode.HTML
+        )
 
     elif query.data == "sticker":
         buttons = [[
